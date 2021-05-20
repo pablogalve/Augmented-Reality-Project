fae5fdc287c5dcb0233811086fffefddb3ed6b1c
Pablo Galve <pablogalve100@gmail.com>
2021-05-20 12:45:11 +0200

    Configurate image targets and add some assets

---
+++ Project/Augmented Reality Project/Assets/Scripts/Joystick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joystick : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveCharacter(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+    }
+
+    void moveCharacter(Vector2 direction){
+        player.Translate(direction * speed * Time.deltaTime);
+    }
+}
