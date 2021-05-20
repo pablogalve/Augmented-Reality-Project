5168820644f40d680e9d6862379dde6962eb780d
Pablo Galve <pablogalve100@gmail.com>
2021-05-20 14:02:25 +0200

    Joystick added but not finished (WIP)
    Co-Authored-By: maciadalmau <47743853+maciadalmau@users.noreply.github.com>

--- Project/Augmented Reality Project/Assets/Scripts/Joystick.cs
+++ Project/Augmented Reality Project/Assets/Scripts/Joystick.cs
@@ -2,24 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Joystick : MonoBehaviour
-{
+public class Joystick : MonoBehaviour {
     public Transform player;
-    public float speed = 0.1f;
+    public float speed = 5.0f;
+    private bool touchStart = false;
+    private Vector2 pointA;
+    private Vector2 pointB;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    public Transform circle;
+    public Transform outerCircle;
+
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0)){
+            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+
+            circle.transform.position = pointA * -1;
+            outerCircle.transform.position = pointA * -1;
+            //circle.GetComponent<SpriteRenderer>().enabled = true;
+            //outerCircle.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        if(Input.GetMouseButton(0)){
+            touchStart = true;
+            pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+        }else{
+            touchStart = false;
+        }
         
-    }
+	}
+	private void FixedUpdate(){
+        if(touchStart){
+            Vector2 offset = pointB - pointA;
+            Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+            moveCharacter(direction * -1);
 
-    // Update is called once per frame
-    void Update()
-    {
-        moveCharacter(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
-    }
+            circle.transform.position = new Vector2(pointA.x + direction.x, pointA.y + direction.y) * -1;
+        }else{
+            //circle.GetComponent<SpriteRenderer>().enabled = false;
+            //outerCircle.GetComponent<SpriteRenderer>().enabled = false;
+        }
 
-    void moveCharacter(Vector2 direction){
+	}
+	void moveCharacter(Vector2 direction){
         player.Translate(direction * speed * Time.deltaTime);
     }
 }
